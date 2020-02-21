8cbf858e267c8bdec79f4cf21176878b11526315
David Jegat <david.jegat@knplabs.com>
2020-02-21 15:13:33 +0100

    Update tests

--- src/Effect/Auth.test.js
+++ src/Effect/Auth.test.js
@@ -1,16 +1,22 @@
-import * as User from './User'
+import * as Auth from './Auth'
 import * as Eff from 'redux-saga/effects'
-import * as UserState from '../State/User'
+import * as AuthState from '../State/Auth'
 
 test(`
-  it contains a root saga that takeEvery user actions
+  it contains a root saga that takeEvery auth actions
   and launch corresponding saga
 `, () => {
   
 });
 
 test(`
-  it can fetch the roles into the 
+  it can fetch the roles from the API
+`, () => {
+  
+});
+
+test(`
+  it can fetch use user from the API
 `, () => {
   
 });
